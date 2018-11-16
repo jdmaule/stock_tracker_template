e59f82f9f17c833b83a2243bf80bfd1caf1e7e1c
Raghu Betina <raghu@firstdraft.com>
2018-11-16 15:18:59 +0000

    Direct association between users and upvotes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :likes,
+             :class_name => "Upvote",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
