f5059a3741ee2771c4241f17839086d00772a689
Raghu Betina <raghu@firstdraft.com>
2018-11-16 15:20:19 +0000

    Only stock user can modify stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,14 @@
 class StocksController < ApplicationController
+  before_action :current_user_must_be_stock_owner, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_stock_owner
+    stock = Stock.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == stock.owner
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @stocks = Stock.all
 
