9856108344477f2d78d1695cf49e08914b75ef5c
Raghu Betina <raghu@firstdraft.com>
2018-11-16 15:19:17 +0000

    Indirect association between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -14,6 +14,10 @@ class Stock < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :fans,
+             :through => :likes,
+             :source => :user
+
   has_many   :followers,
              :through => :owner,
              :source => :following
